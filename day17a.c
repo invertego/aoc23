4ab3e34d254c2e99fdbbad966ad7f632382119d2
invertego <invertego@users.noreply.github.com>
2023-12-19 15:19:56 -0500

    day 17

---
+++ day17a.c
@@ -0,0 +1,147 @@
+#include <assert.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#define PART 0
+
+#define A(i,j) a[(i)+1][(j)+1]
+#define M(i,j) memo[(i)+1][(j)+1]
+char a[141+2][141+2];
+int memo[141+2][141+2];
+int m, n;
+
+char input[141+2][141+2];
+
+const int big = 10000;
+
+int solve(int i, int j)
+{
+    if (M(i, j) >= 0) return M(i, j);
+    if (i < 0 || j < 0 || i >= m || j >= n) return M(i, j) = big;
+    if (i == m - 1 && j == n - 1) return M(i, j) = 0;
+    int x = solve(i + 1, j) + A(i + 1, j);
+    int y = solve(i - 1, j) + A(i - 1, j);
+    int z = solve(i, j + 1) + A(i, j + 1);
+    int w = solve(i, j - 1) + A(i, j - 1);
+    if (y < x) x = y;
+    if (z < x) x = z;
+    if (w < x) x = w;
+    return M(i, j) = x;
+}
+
+
+#define INFINITY 10000
+#define UNDEFINED (coord_t){-1,-1}
+
+typedef struct {
+    int i, j;
+} coord_t;
+
+#define PUSH(x,y) (open[opencnt++] = (coord_t){(x),(y)})
+coord_t open[100000];
+int opencnt;
+
+int dist[141+2][141+2];
+coord_t prev[141+2][141+2];
+
+const int di[] = {-1, 1, 0, 0};
+const int dj[] = { 0, 0,-1, 1};
+
+int solve2()
+{
+    int mindist = INFINITY;
+    int maxdist = 0;
+
+    for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++) {
+        dist[i][j] = INFINITY;
+        prev[i][j] = UNDEFINED;
+        PUSH(i,j);
+    }
+    dist[0][0] = 0;
+
+    while (opencnt) {
+        int min = INFINITY;
+        int uidx = -1;
+        coord_t u = {0};
+        for (int k = 0; k < opencnt; k++) {
+            coord_t tmp = open[k];
+            int d = dist[tmp.i][tmp.j];
+            if (min > d) {
+                min = d;
+                u = tmp;
+                uidx = k;
+            }
+        }
+        memmove(&open[uidx], &open[uidx + 1], sizeof(open[0]) * (opencnt - uidx - 1));
+        opencnt--;
+        
+        for (int k = 0; k < 4; k++) {
+            coord_t v = u;
+            v.i += di[k];
+            v.j += dj[k];
+            //test v in Q
+            if (v.i < 0 || v.i >= m ||
+                v.j < 0 || v.j >= n) {
+                continue;
+            }
+            int alt = dist[u.i][u.j] + A(v.i,v.j);//Edges(u, v)
+            if (alt < mindist) mindist = alt;
+            if (alt > maxdist) maxdist = alt;
+            if (alt < dist[v.i][v.j]) {
+                dist[v.i][v.j] = alt;
+                prev[v.i][v.j] = u;
+            }
+        }
+    }
+
+    coord_t S[1000];
+    int Sidx = 0;
+    coord_t u = {m-1,n-1};
+    if (prev[u.i][u.j].i >= 0 || (u.i == 0 && u.j == 0)) {
+        while (u.i >= 0) {
+            input[u.i][u.j] = '*';
+            S[Sidx++] = u;
+            u = prev[u.i][u.j];
+        }
+    }
+
+    printf("min %d max %d\n", mindist, maxdist);
+
+    return dist[m-1][n-1];
+}
+
+int main()
+{
+    FILE* f = fopen("day17.txt", "r");
+    char b[256];
+    int64_t sum = 0, sum2 = 0;
+
+    while (fgets(b, sizeof(b), f)) {
+        *strchr(b, '\n') = 0;
+        n = strlen(b);
+        memcpy(&input[m], b, n);
+        for (int i = 0; i < n; i++) {
+            A(m,i) = b[i] - '0';
+        }
+        m++;
+    }
+
+    //memset(memo, -1, sizeof(memo));
+    //sum = solve(0, 0);
+    sum = solve2();
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%c", input[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n%lld\n", sum);
+    printf("\n%lld\n", sum2);
+}
---
+++ day17b.c
@@ -0,0 +1,171 @@
+#include <assert.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#define PART 0
+
+#define A(i,j) a[(i)+1][(j)+1]
+char a[141+2][141+2];
+int m, n;
+
+char input[141+2][141+2];
+
+#define INFINITY 10000
+#define UNDEFINED (key_t){.v = ~0}
+
+#define CAP (1 << 20)
+//#define KEY(i, j, k, l) (((i) << 12) | ((j) << 4) | ((k) << 2) | (l))
+#define KEY(i, j, k, l) ((key_t){{(l),(k),(j),(i)}})
+
+typedef union {
+    struct {
+        unsigned l : 2;
+        unsigned k : 2;
+        unsigned j : 8;
+        unsigned i : 8;
+        unsigned _ : 12;
+    };
+    unsigned v;
+} key_t;
+
+typedef struct {
+    int i, j;
+} coord_t;
+
+#define PUSH(key) (open[opencnt++] = (key))
+key_t open[CAP];
+int opencnt;
+
+int dist[CAP];
+key_t prev[CAP];
+
+const int di[] = {-1, 1, 0, 0};
+const int dj[] = { 0, 0,-1, 1};
+
+int solve2()
+{
+    //int count = 0;
+    for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++)
+    for (int k = 0; k < 4; k++)
+    for (int l = 0; l < 3; l++) {
+        key_t key = KEY(i,j,k,l);
+        //if (count++ < 100) printf("%d %d %d %d %x\n", key.i, key.j, key.k, key.l, key.v);
+        dist[key.v] = (i == 0 && j == 0) ? 0 : INFINITY;
+        prev[key.v] = UNDEFINED;
+        PUSH(key);
+    }
+
+    int mindist = INFINITY;
+    int maxdist = 0;
+    int count = 0;
+    while (opencnt) {
+        if (!(count++ % 1024)) printf("opencnt %d\n", opencnt);
+        int min = INFINITY*100;
+        int uidx = -1;
+        key_t u;
+        for (int idx = 0; idx < opencnt; idx++) {
+            key_t tmp = open[idx];
+            int d = dist[tmp.v];
+            if (min > d) {
+                min = d;
+                u = tmp;
+                uidx = idx;
+            }
+        }
+        assert(uidx >= 0);
+        memmove(&open[uidx], &open[uidx + 1], sizeof(open[0]) * (opencnt - uidx - 1));
+        opencnt--;
+        
+        for (int k = 0; k < 4; k++) {
+            key_t v = u;
+            v.i += di[k];
+            v.j += dj[k];
+            v.k = k;
+            if (k == u.k) {
+                if (++v.l == 3) continue;
+            } else if (k == (u.k ^ 1)) {
+                continue;
+            } else {
+                v.l = 0;
+            }
+            //test v in Q
+            if (v.i < 0 || v.i >= m ||
+                v.j < 0 || v.j >= n) {
+                continue;
+            }
+            int alt = dist[u.v] + A(v.i,v.j);//Edges(u, v)
+            if (alt < mindist) mindist = alt;
+            if (alt > maxdist && alt < INFINITY) maxdist = alt;
+            if (alt < dist[v.v]) {
+                dist[v.v] = alt;
+                prev[v.v] = u;
+            }
+        }
+    }
+
+    int shortest = INFINITY;
+    void* backup = malloc(sizeof(input));
+    memcpy(backup, input, sizeof(input));
+    for (int k = 0; k < 4; k++)
+    for (int l = 0; l < 3; l++) {
+        memcpy(input, backup, sizeof(input));
+
+        int d = dist[KEY(m-1,n-1,k,l).v];
+        if (shortest > d) shortest = d;
+        printf("dist %d\n", d);
+        key_t S[1000];
+        int Sidx = 0;
+        key_t u = KEY(m-1,n-1,k,l);
+        if (prev[u.v].v != UNDEFINED.v || (u.i == 0 && u.j == 0)) {
+            while (u.v != UNDEFINED.v) {
+                input[u.i][u.j] =
+                    "^v<>"[u.k];
+                    //"0123"[u.l];
+                S[Sidx++] = u;
+                u = prev[u.v];
+            }
+        
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    printf("%c", input[i][j]);
+                }
+                printf("\n");
+            }
+        }
+    }
+
+    printf("min %d max %d\n", mindist, maxdist);
+
+    //return dist[m-1][n-1];
+
+    return shortest;
+}
+
+int main()
+{
+    FILE* f = fopen("day17.txt", "r");
+    char b[256];
+    int64_t sum = 0, sum2 = 0;
+
+    while (fgets(b, sizeof(b), f)) {
+        *strchr(b, '\n') = 0;
+        n = strlen(b);
+        memcpy(&input[m], b, n);
+        for (int i = 0; i < n; i++) {
+            A(m,i) = b[i] - '0';
+        }
+        m++;
+    }
+
+    //memset(memo, -1, sizeof(memo));
+    //sum = solve(0, 0);
+    sum = solve2();
+
+    printf("\n%lld\n", sum);
+    printf("\n%lld\n", sum2);
+}
